7fa487a037d847aa67d8a6554a85cc25c7e1875e
Cian John <topjohncian@gmail.com>
2020-08-05 23:41:59 +0800

    Feat: 增加 一些Bug ~请求明天早上有大佬帮我修复~

--- control/websocket.py
+++ control/websocket.py
@@ -1,15 +1,32 @@
 import json
+import threading
 
-from websocket import create_connection
+import websocket
 
 from config import POIMANAGER_API_WS, POIMANAGER_API_TOKEN
 
-connection = create_connection(POIMANAGER_API_WS)
 
+class WebSocket:
+    def __init__(self):
+        self.auth = False
+        self.ws = websocket.WebSocketApp(POIMANAGER_API_WS,
+                                         on_close=self.on_close,
+                                         on_message=self.on_message)
+        self.wsthread = None
 
-def createConnection():
-    connection.send(json.load({'token': POIMANAGER_API_TOKEN}))
-    result = connection.recv()
-    res = json.loads(result)
-    if (res.type != 'auth') or (res.msg != 'OK') or (res.code != 200):
-        print("poimanager auth error:", res.msg)
+    def on_open(self, ws):
+        self.ws.send(json.load({'token': POIMANAGER_API_TOKEN}))
+
+    def on_message(self, ws, message):
+        res = json.loads(message)
+        print(res)
+        if (res.type == 'auth') and (res.msg == 'OK'):
+            self.auth = True
+            return
+
+    def on_close(self):
+        self.run()
+
+    def run(self):
+        self.wsthread = threading.Thread(target=self.ws.run_forever)
+        self.wsthread.run()
