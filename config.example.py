6d48b1c14d910b188f55e5a352319786c342688d
Cian John <topjohncian@gmail.com>
2020-08-05 17:32:28 +0800

    feat: 增加数据库链接 [NEED UPDATE DEPS]

--- config.example.py
+++ config.example.py
@@ -1,5 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 BOT_TOKEN = ''
-REQUEST_KWARGS={
+REQUEST_KWARGS = {
     # "USERNAME:PASSWORD@" is optional, if you need authentication:
     'proxy_url': 'http://127.0.0.1:7890/',
 }
+DB_HOST = '1.1.1.1'
+DB_USER = 'potbot'
+DB_PASS = 'wdnmdcnmlgb'
+DB_NAME = 'cao'
+
+engine = create_engine(
+    'mysql+pymysql://%s:%s@%s/%s' % (DB_USER, DB_PASS, DB_HOST, DB_NAME), echo=True)
+
+SESSION = sessionmaker(bind=engine)
