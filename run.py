dab922db3e0565289fe29b1987340adfa3cceab2
hackbiter <62537288+hackbiter@users.noreply.github.com>
2020-08-05 13:49:58 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -1,64 +1,20 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-First, a few callback functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Example of a bot-user conversation using ConversationHandler.
-Send /start to initiate the conversation.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
-import token
-import Hitokoto
+from telegram.ext import CommandHandler
+import key
+from telegram.ext import Updater
 
-from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
-                          ConversationHandler)
-
-# Enable logging
+updater = Updater(token=key.token, use_context=True)
+dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-logger = logging.getLogger(__name__)
-
 
 def yy(update, context):
-    update.message.reply_text('Hitokoto.text')
-
-def main():
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater("token.token", use_context=True)
+    context.bot.send_message(chat_id=update.effective_chat.id, text='test')
 
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
+start_handler = CommandHandler('yy', yy)
+dispatcher.add_handler(start_handler)
+updater.start_polling()
 
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-
-        
-        fallbacks=[CommandHandler('bye', bye)]
-    )
-
-    dp.add_handler(conv_handler)
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+updater.stop()
