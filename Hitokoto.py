5ea98bd71ecf47cbcf29859fe934a413217fb2f5
Cian John <topjohncian@gmail.com>
2020-08-05 16:10:53 +0800

    refactor project

--- Hitokoto.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-
-
-Return = requests.get('https://v1.hitokoto.cn/?c=a')
-Json = json.loads(Return.text)
--- run.py
+++ run.py
@@ -1,22 +1,13 @@
 import logging
-from telegram.ext import CommandHandler
-import key
-import importlib
 from telegram.ext import Updater
-import Hitokoto
+from config import BOT_TOKEN
+from command import command
 
-updater = Updater(token=key.token, use_context=True)
+updater = Updater(token=BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-
-def yy(update, context):
-    importlib.reload(Hitokoto)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=Hitokoto.Return.text)
-
-
-start_handler = CommandHandler('yy', yy)
-dispatcher.add_handler(start_handler)
+command.add_handler(dispatcher)
 updater.start_polling()
 updater.idle()
