7235c16a5d525a2675b6f26de5489c4b1e1ebdf4
Cian John <topjohncian@gmail.com>
2020-08-06 15:34:07 +0800

    Feat: 插件适配 PoiManager - HTTP API

--- command/addWhiteList.py
+++ command/addWhiteList.py
@@ -1,10 +1,10 @@
-from config import SERVER_HOST, CHAT_ID
-from websocket import create_connection
+from telegram import Update, Chat
 from telegram.ext import CommandHandler, CallbackContext
-from telegram import Update, Chat, ChatMember
-import time
 
-chat = Chat(CHAT_ID, 'group')
+from config import TELEGRAM_CHAT_ID
+from control.command import sendCommand
+
+chat = Chat(TELEGRAM_CHAT_ID, 'group')
 
 
 def addWhiteList(update: Update, context: CallbackContext):
@@ -13,26 +13,23 @@ def addWhiteList(update: Update, context: CallbackContext):
     if user:
         if chat.get_member(user.id).status == 'creator' or chat.get_member(user.id).status == 'administrator':
             if len(args) != 0:
-                try:
-                    ws = create_connection(SERVER_HOST)
-                except:
+                res = sendCommand('whitelist add %s' % args[0], '1')
+                if not res.status:
                     context.bot.send_message(chat_id=update.effective_chat.id,
-                                             text='服务器去火星了,等会儿再试试吧!',
+                                             text='服务器去火星了,等会儿再试试吧! 错误信息:' % res.msg,
                                              reply_to_message_id=update.effective_message.id)
-                ws.send('whitelist add %s' % args[0])
-                time.sleep(0.1)
-                result = ws.recv()
-                if result == "Player added to whitelist":
+
+                if res.content[0] == "Player added to whitelist":
                     context.bot.send_message(
-                        chat_id=update.effective_chat.id, text=('已经把%s添加到Poicraft的白名单中了呢!' % args[0]),
+                        chat_id=update.effective_chat.id, text=('已经把%s添加到PoiCraft的白名单中了呢!' % args[0]),
                         reply_to_message_id=update.effective_message.id)
-                elif result == 'Player already in whitelist':
+                elif res.content[0] == 'Player already in whitelist':
                     context.bot.send_message(chat_id=update.effective_chat.id,
-                                             text=('%s已经在Poicraft的白名单中了呢!' % args[0]),
+                                             text=('%s已经在PoiCraft的白名单中了呢!' % args[0]),
                                              reply_to_message_id=update.effective_message.id)
                 else:
                     context.bot.send_message(chat_id=update.effective_chat.id,
-                                             text='出了点问题?试试提个issues???',
+                                             text='出了点问题?试试提个issues??? 错误信息:' % res.msg,
                                              reply_to_message_id=update.effective_message.id)
             elif len(args) == 0:
                 context.bot.send_message(chat_id=update.effective_chat.id,
@@ -44,4 +41,4 @@ def addWhiteList(update: Update, context: CallbackContext):
                                          reply_to_message_id=update.effective_message.id)
 
 
-addWhiteListHandler = CommandHandler('addWhiteList', addWhiteList, allow_edited=False)
+addWhiteListHandler = CommandHandler('addWhiteList', addWhiteList)
