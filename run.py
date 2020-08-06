28c0954dea19e623cd89421ab9a3cbebd28bc362
Cian John <topjohncian@gmail.com>
2020-08-06 15:36:05 +0800

    Merge branch 'topjohncian' of github.com:PoiCraft/PoiTgBot-Py into topjohncian
    # Conflicts:
    #	run.py

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ from telegram.ext import Updater
 
 from command import command
 from config import BOT_TOKEN, REQUEST_KWARGS
-from control.websocket import WebSocket
 
 updater = Updater(token=BOT_TOKEN, use_context=True, request_kwargs=REQUEST_KWARGS)
 dispatcher = updater.dispatcher
@@ -12,7 +11,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 command.add_handler(dispatcher)
-WebSocket().run()
 print('Running...')
 updater.start_polling()
 updater.idle()
