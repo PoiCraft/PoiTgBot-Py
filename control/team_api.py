2a8f3c9e8f610f6bc0d16a5e05206347c38e1db7
lostDeers <50144988+lostDeers@users.noreply.github.com>
2020-08-07 15:13:06 +0800

    add team_api.py improve database.py

---
+++ control/team_api.py
@@ -0,0 +1,43 @@
+from database.database import scoped_session, Player, Team
+from sqlalchemy.orm.exc import NoResultFound
+
+
+def create_team(leaderID: int):
+    try:
+        session = scoped_session()
+        new_team = Team(leaderID=leaderID)
+        session.add(new_team)
+        player = session.query(Player).filter(Player.TelegramID == leaderID).one()
+        player.TeamIN = new_team
+        session.commit()
+        session.close()
+        return True
+    except NoResultFound:
+        return False
+
+
+def join_team(userID: int, teamID: int = None, leaderID: int = None):
+    try:
+        session = scoped_session()
+        if teamID is not None:
+            team = session.query(Team).filter(Team.ID == teamID).one()
+        elif leaderID is not None:
+            team = session.query(Player).filter(Player.TelegramID == leaderID).one().TeamIN
+        player = session.query(Player).filter(Player.TelegramID == userID).one()
+        player.TeamIN = team
+        session.commit()
+        session.close()
+        return True
+    except NoResultFound:
+        return False
+
+
+def leave_team(userID: int):
+    try:
+        session = scoped_session()
+        session.query(Player).filter(Player.TelegramID == userID).one().TeamIN = None
+        session.commit()
+        session.close()
+        return True
+    except NoResultFound:
+        return False
